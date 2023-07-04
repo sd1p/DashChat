dd99a8333becb1237cfc26caceecad0e387075e9
Sudip Mandal <skmxyt@gmail.com>
2023-07-05 01:16:25 +0530

    edited vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,9 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-
 export default defineConfig({
   server: {
     proxy: {
