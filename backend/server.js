2970d742d6fe805ed550ac18f09c92c40f02a1f1
Sudip Mandal <43758514+sd1p@users.noreply.github.com>
2023-07-04 23:36:48 +0530

    updated cors policy on server

--- backend/server.js
+++ backend/server.js
@@ -45,7 +45,7 @@ const server = app.listen(PORT, console.log(`Server Started on port ${PORT}`));
 const io = require("socket.io")(server, {
   pingTimeout: 60000,
   cors: {
-    origin: "http://localhost:5173",
+    origin: "https://dash-chat-five.vercel.app/",
   },
 });
 
