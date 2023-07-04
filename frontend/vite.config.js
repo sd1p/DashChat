60948b9874d6dfb16444520f6a97287c26cf0b97
Sudip Mandal <skmxyt@gmail.com>
2023-07-05 01:36:09 +0530

    reconfig vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,33 +3,33 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      "/api": {
-        target: "https://dashchat-backend.onrender.com/",
-        changeOrigin: true,
-        secure: true,
-        ws: true,
-        rewrite: (path) => path.replace(/^\/api/, ""),
-      },
-    },
-  },
-});
 // export default defineConfig({
+//   plugins: [react()],
 //   server: {
-//     host: true,
 //     proxy: {
 //       "/api": {
 //         target: "https://dashchat-backend.onrender.com/",
 //         changeOrigin: true,
-//         secure: false,
+//         secure: true,
+//         ws: true,
 //       },
 //     },
 //   },
-//   plugins: [react()],
 // });
+export default defineConfig({
+  server: {
+    host: true,
+    proxy: {
+      "/api": {
+        target: "https://dashchat-backend.onrender.com/",
+        changeOrigin: true,
+        secure: false,
+        ws: true,
+      },
+    },
+  },
+  plugins: [react()],
+});
 
 //dev config
 // export default defineConfig({
