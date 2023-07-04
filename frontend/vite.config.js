9ea99b1f8148d5c62b09c551ebef8b4662db906c
Sudip Mandal <skmxyt@gmail.com>
2023-07-05 00:49:40 +0530

    changed proxy config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,19 +2,36 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
+
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
 export default defineConfig({
   server: {
-    host: true,
     proxy: {
       "/api": {
         target: "https://dashchat-backend.onrender.com/",
         changeOrigin: true,
-        secure: false,
+        secure: true,
+        rewrite: (path) => path.replace(/^\/api/, ""),
       },
     },
   },
   plugins: [react()],
 });
+// export default defineConfig({
+//   server: {
+//     host: true,
+//     proxy: {
+//       "/api": {
+//         target: "https://dashchat-backend.onrender.com/",
+//         changeOrigin: true,
+//         secure: false,
+//       },
+//     },
+//   },
+//   plugins: [react()],
+// });
 
 //dev config
 // export default defineConfig({
