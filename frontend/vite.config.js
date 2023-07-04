ba1d6ffdc1ecea316474d4064fe3106952be608b
Sudip Mandal <skmxyt@gmail.com>
2023-07-04 23:38:48 +0530

    updated vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,7 +5,6 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   server: {
     host: true,
-    port: 5173,
     proxy: {
       "/api": {
         target: "https://dashchat-backend.onrender.com/",
