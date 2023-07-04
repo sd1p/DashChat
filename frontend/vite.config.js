0a090afae5b2673c10cd1d4d78b1097864747373
Sudip Mandal <skmxyt@gmail.com>
2023-07-05 01:27:03 +0530

    edited vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "https://dashchat-backend.onrender.com",
+        target: "https://dashchat-backend.onrender.com/",
         changeOrigin: true,
         secure: true,
         ws: true,
