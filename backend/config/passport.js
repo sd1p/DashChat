8662b77691d65b424a9d41195735ca7d2e600d31
Sudip Mandal <skmxyt@gmail.com>
2023-07-10 02:56:53 +0530

    changed callback url

--- backend/config/passport.js
+++ backend/config/passport.js
@@ -22,8 +22,7 @@ module.exports = (passport) => {
       {
         clientID: process.env.GoogleClientID,
         clientSecret: process.env.GoogleClientSecret,
-        callbackURL:
-          "/api/auth/google/callbackhttps://dashchat.onrender.com/api/auth/google/callback",
+        callbackURL: "https://dashchat.onrender.com/api/auth/google/callback",
         // passReqToCallback: true, // allows us to pass in the req from our router.
       },
       async (req, token, refreshToken, profile, done) => {
